84c405b4fdf6dd4e5bd5bc36c6ca6949055faba8
Tom <tnorth93@gmail.com>
2018-09-14 14:05:39 -0700

    fixed array shift challenge code, added tests

--- __test__/shift.test.js
+++ __test__/shift.test.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const shiftArray = require('../array_shift.js');
+const shiftArray = require('../lib/array_shift.js');
 
 describe('#shift.test.js', () => {
-    describe('testing the array shift module', () => {
-        test('should return an array with the given value inserted into the middle index position of the original array', () => {
-            const insertedValue = shiftArray.insertShiftArray([1,2,3,4,5,6], 0);
-            expect(insertedValue).toEqual([1,2,3,0,4,5,6]);
-        });
-        test('should return an array with just the value to be added if the array given was empty', () => {
-            const onlyArrayValue = shiftArray.insertShiftArray([], 11);
-            expect(onlyArrayValue).toEqual([11]);
-        });
+  describe('testing the array shift module', () => {
+    test('should return an array with the given value inserted into the middle index position of the original array', () => {
+      const insertedValue = shiftArray.insertShiftArray([1, 2, 3, 4, 5, 6], 0);
+      expect(insertedValue).toEqual([1, 2, 3, 0, 4, 5, 6]);
     });
+    test('should return an array with just the value to be added if the array given was empty', () => {
+      const onlyArrayValue = shiftArray.insertShiftArray([], 11);
+      expect(onlyArrayValue).toEqual([11]);
+    });
+  });
 });
