6382361a5f6c567808b05c44b7fc15130950e698
Tom <tnorth93@gmail.com>
2018-09-14 11:39:55 -0700

    finished testing for array shift

--- array_shift.js
+++ array_shift.js
@@ -1,5 +1,9 @@
-function insertShiftArray(arr, val) {
-  if (arr.length < 1) {
+'use strict';
+
+const shiftArray = module.exports = {};
+
+shiftArray.insertShiftArray = (arr, val) => {
+  if (arr.length <= 1) {
     arr.push(val);
     return arr;
   } else {
@@ -9,4 +13,3 @@ function insertShiftArray(arr, val) {
   }
 }
 
-insertShiftArray([1,2,3,4,5,6,], 0);
